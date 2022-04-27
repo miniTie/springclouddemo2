f4104c7b1f92dd448b8e29b6c89e6fc1230712a2
yangzhen <yangz6493@gmail.com>
2022-04-27 14:44:29 +0800

    增加springcloudconfigclient

---
+++ springcloudconfigclient/src/test/java/pers/young/springcloudconfigclient/SpringcloudconfigclientApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.young.springcloudconfigclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudconfigclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
